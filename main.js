7d38fab0f8391e7bcee998e9b2a9e24e1b0d1d08
Chihli0802 <annannwhat@gmail.com>
2023-04-21 17:06:06 +0800

    done

---
+++ main.js
@@ -0,0 +1,562 @@
+//物件 divorce
+const divorce ={
+    "success": true,
+    "result": {
+    "resource_id": "301000000A-002072-001",
+    "limit": 2000,
+    "total": 45,
+    "fields": [
+    {
+    "type": "text",
+    "id": "statistic_yyy"
+    },
+    {
+    "type": "text",
+    "id": "according"
+    },
+    {
+    "type": "text",
+    "id": "district_code"
+    },
+    {
+    "type": "text",
+    "id": "site_id"
+    },
+    {
+    "type": "text",
+    "id": "sex"
+    },
+    {
+    "type": "text",
+    "id": "average_age"
+    }
+    ],
+    "records": [
+    {
+    "statistic_yyy": "統計年",
+    "according": "按照別",
+    "district_code": "區域別代碼",
+    "site_id": "區域別",
+    "sex": "性別",
+    "average_age": "平均年齡"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "65000000",
+    "site_id": "新北市",
+    "sex": "男",
+    "average_age": "33.0"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "65000000",
+    "site_id": "新北市",
+    "sex": "女",
+    "average_age": "31.1"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "63000000",
+    "site_id": "臺北市",
+    "sex": "男",
+    "average_age": "33.9"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "63000000",
+    "site_id": "臺北市",
+    "sex": "女",
+    "average_age": "32.3"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "68000000",
+    "site_id": "桃園市",
+    "sex": "男",
+    "average_age": "31.9"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "68000000",
+    "site_id": "桃園市",
+    "sex": "女",
+    "average_age": "30.0"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "66000000",
+    "site_id": "臺中市",
+    "sex": "男",
+    "average_age": "32.0"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "66000000",
+    "site_id": "臺中市",
+    "sex": "女",
+    "average_age": "30.2"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "67000000",
+    "site_id": "臺南市",
+    "sex": "男",
+    "average_age": "32.5"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "67000000",
+    "site_id": "臺南市",
+    "sex": "女",
+    "average_age": "30.5"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "64000000",
+    "site_id": "高雄市",
+    "sex": "男",
+    "average_age": "32.5"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "64000000",
+    "site_id": "高雄市",
+    "sex": "女",
+    "average_age": "30.5"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10002000",
+    "site_id": "宜蘭縣",
+    "sex": "男",
+    "average_age": "31.4"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10002000",
+    "site_id": "宜蘭縣",
+    "sex": "女",
+    "average_age": "29.5"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10004000",
+    "site_id": "新竹縣",
+    "sex": "男",
+    "average_age": "31.8"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10004000",
+    "site_id": "新竹縣",
+    "sex": "女",
+    "average_age": "30.0"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10005000",
+    "site_id": "苗栗縣",
+    "sex": "男",
+    "average_age": "31.6"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10005000",
+    "site_id": "苗栗縣",
+    "sex": "女",
+    "average_age": "29.8"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10007000",
+    "site_id": "彰化縣",
+    "sex": "男",
+    "average_age": "31.3"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10007000",
+    "site_id": "彰化縣",
+    "sex": "女",
+    "average_age": "29.3"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10008000",
+    "site_id": "南投縣",
+    "sex": "男",
+    "average_age": "31.0"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10008000",
+    "site_id": "南投縣",
+    "sex": "女",
+    "average_age": "29.2"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10009000",
+    "site_id": "雲林縣",
+    "sex": "男",
+    "average_age": "31.1"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10009000",
+    "site_id": "雲林縣",
+    "sex": "女",
+    "average_age": "29.1"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10010000",
+    "site_id": "嘉義縣",
+    "sex": "男",
+    "average_age": "31.2"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10010000",
+    "site_id": "嘉義縣",
+    "sex": "女",
+    "average_age": "29.3"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10013000",
+    "site_id": "屏東縣",
+    "sex": "男",
+    "average_age": "31.7"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10013000",
+    "site_id": "屏東縣",
+    "sex": "女",
+    "average_age": "29.4"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10014000",
+    "site_id": "臺東縣",
+    "sex": "男",
+    "average_age": "31.2"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10014000",
+    "site_id": "臺東縣",
+    "sex": "女",
+    "average_age": "29.5"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10015000",
+    "site_id": "花蓮縣",
+    "sex": "男",
+    "average_age": "31.3"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10015000",
+    "site_id": "花蓮縣",
+    "sex": "女",
+    "average_age": "29.3"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10016000",
+    "site_id": "澎湖縣",
+    "sex": "男",
+    "average_age": "31.2"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10016000",
+    "site_id": "澎湖縣",
+    "sex": "女",
+    "average_age": "29.3"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10017000",
+    "site_id": "基隆市",
+    "sex": "男",
+    "average_age": "32.4"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10017000",
+    "site_id": "基隆市",
+    "sex": "女",
+    "average_age": "30.6"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10018000",
+    "site_id": "新竹市",
+    "sex": "男",
+    "average_age": "32.6"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10018000",
+    "site_id": "新竹市",
+    "sex": "女",
+    "average_age": "30.7"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10020000",
+    "site_id": "嘉義市",
+    "sex": "男",
+    "average_age": "32.4"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "10020000",
+    "site_id": "嘉義市",
+    "sex": "女",
+    "average_age": "30.1"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "09020000",
+    "site_id": "金門縣",
+    "sex": "男",
+    "average_age": "31.7"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "09020000",
+    "site_id": "金門縣",
+    "sex": "女",
+    "average_age": "30.1"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "09007000",
+    "site_id": "連江縣",
+    "sex": "男",
+    "average_age": "32.0"
+    },
+    {
+    "statistic_yyy": "110",
+    "according": "按發生日期分",
+    "district_code": "09007000",
+    "site_id": "連江縣",
+    "sex": "女",
+    "average_age": "30.0"
+    }
+    ]
+    }
+    }
+console.log(divorce.result.records[1].average_age);
+
+//列出所有城市及性別及平均年齡
+// divorce.result.records.forEach(function(i){
+//     console.log(i.site_id,i.sex,i.average_age);
+// });
+
+
+
+
+
+//物件 obj 包含了方法的物件
+const na = "全域變數"; //注意變數的範圍，{}中的範圍
+const obj = {
+    na: "keroro",
+    fn: function(){
+        console.log("this is obj's function")
+    }  // 執行方法，實際上沒有回傳值，因此會出現
+}
+//呼叫物件中的方法
+console.log(obj.na)
+console.log(obj.fn());  //加小括號
+console.log(na);
+
+// 變數範圍
+//{}中如果有宣告變數，就是呼叫{}中的
+//{}沒有宣告變數的話，則呼叫宣告為全域變數的數
+
+
+
+
+
+
+
+
+//物件 cat
+const cat = {
+    name: "mimi",
+    tel: [
+        "0978-229-161",
+        "0910-894-434",
+        22222222
+    ],
+    age: 5,
+    email: [
+        "ann@gmail",
+        "midori@gmail"
+    ],                   //物件內的value可以是陣列
+    address: "tonarino",
+
+    pet: [{
+        name: "yuyu",
+        age: 44
+    }, {
+        name: "lili",
+        age: 26,
+        tel: [
+            "000-3333-6666",
+            "000-4444-5555"
+        ]
+    }]
+}
+// 取值方法一 較少用 ==================================
+// console.log(cat);
+// console.log(cat["age"]); //需轉成字串
+// console.log(cat["email"]); //丟key，會回傳value
+
+//取值方法二 ==========================================
+console.log(cat.address); //物件.key值 => 取得value
+console.log(cat.tel[1]);  //資料是陣列的話，用index找
+
+// 取得多重資料 =======================================
+// 用{}的話，裡面一定要是key and value
+console.log(cat.pet[0].age); //遇到{}就是. []就是index
+
+//會有進位
+console.log(cat.tel[2]) 
+
+// 一次列出陣列裡的資料，用foreach ====================
+cat.email.forEach(function(i){
+    console.log(i);
+})
+
+// 叫出 所有物件的同個資訊 =============================
+cat.pet.forEach(function(i){
+    console.log(i.name); //i是物件 i.name物件中的name
+})
+
+
+
+
+
+// 老師的餐點
+const mealList= [
+    {
+        "name": "食在一方",
+        "category": ["brunch"],
+        "filter": false
+    },
+    {
+        "name": "日十",
+        "category": ["brunch"],
+        "filter": false
+    },
+    {
+        "name": "鹽行站",
+        "category": ["buffet", "rice"],
+        "filter": false
+    },
+    {
+        "name": "鍋燒麵",
+        "category": ["noodle"],
+        "filter": false
+    },
+    {
+        "name": "炒飯",
+        "category": ["rice"],
+        "filter": false
+    },
+    {
+        "name": "八方雲集",
+        "category": ["others"],
+        "filter": false
+    },
+    {
+        "name": "火鍋",
+        "category": ["others"],
+        "filter": false
+    },
+    {
+        "name": "韓式料理",
+        "category": ["rice", "noodle"],
+        "filter": false
+    },
+    {
+        "name": "小飯糰大飯糰",
+        "category": ["rice"],
+        "filter": false
+    },
+    {
+        "name": "後校門滷肉飯",
+        "category": ["rice", "noodle"],
+        "filter": false
+    },
+    {
+        "name": "魚耶",
+        "category": ["brunch", "rice", "noodle"],
+        "filter": false
+    },
+    {
+        "name": "吳家鴨香飯",
+        "category": ["rice", "noodle"],
+        "filter": false
+    }
+]
+
+console.log(mealList[5].name);
+
+mealList.forEach(function(i){
+    console.log(i.name);
+})
+
+//隨機 顯示一間店名
+const random = Math.floor(Math.random()*mealList.length); //1~12
+console.log("隨機:",mealList[random].name);
